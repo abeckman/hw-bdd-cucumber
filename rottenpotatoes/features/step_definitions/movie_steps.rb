2b71c458366cd53ae14d4d4cf98326e2be72800d
Art Beckman <aabeckman@yahoo.com>
2015-11-23 05:02:07 +0000

    completed hw3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,10 +2,10 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    new_movie = Movie.create!(movie)
+# new_movie = Movie.create!(:title => movie["title"], :rating => movie["rating"], :release_date => movie["release_date"])
   end
-  fail "Unimplemented"
+# fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  regexp = /#{e1}.*#{e2}/m # /m means match across newlines
+#page.body.should =~ regexp
+  expect(page.body).to match(regexp)
+# fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +28,51 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck
+    rating_list.split(', ').each {|rating|
+      steps %Q{
+        When I uncheck "ratings[#{rating}]"
+      }
+    }
+  else
+    rating_list.split(', ').each {|rating|
+      steps %Q{
+        When I check "ratings[#{rating}]"
+      }
+    }
+  end
+
+#  fail "Unimplemented"
 end
 
-Then /I should see all the movies/ do
+Then /I should (not )?see the following ratings: (.*)/ do |shouldnot, rating_list|
+  if shouldnot
+    rating_list.split(', ').each {|rating|
+      steps %Q{
+        Then I should not see the movies rated "#{rating}"
+      }
+    }
+  else
+    rating_list.split(', ').each {|rating|
+      steps %Q{
+        Then I should see the movies rated "#{rating}"
+      }
+    }
+  end
+end
+
+Then /I should (not )?see the movies rated (.*)/ do |shouldnot, rating|
+  if shouldnot
+    page.find('table#movies').has_no_text?("#{rating}")
+  else
+    @rating_count = Movie.where(:rating => "#{rating}" ).count
+    @table_rating = page.all('table#movies', text: "#{rating}").count
+    @rating_count == @table_rating
+  end
+end
+
+Then /I should see all of the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(page.all('table#movies tr').count).to eq(Movie.count + 1) # extra 1 for header
+# fail "Unimplemented"
 end
